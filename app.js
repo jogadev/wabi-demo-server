77da6580c1527bfdd9d4d48d33a3e8ce5b77e90d
Jorge Garcia <jogadev@outlook.com>
2020-06-12 09:23:36 -0500

    Connecting to local db

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require("cors");
+const connectMongo = require('./mongodb');
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.get("/random", (req, res) => {
+    const gameList = ["The Last of Us 2", "God of War", "Horizon Zero Dawn", "Spiderman", "GTA V", "Hollow Knight", "Super Smash Bros Ultimate", "Mario Kart 8", "Call of Duty"];
+    const salesList = [];
+    for(var game in gameList){
+        var price = Math.round((Math.random() * 300) + 700) // [700,1000]
+        
+        var cost = Math.round((Math.random() * (300)) + (price - 500)) // [price-500, price-200] 
+
+        var units = Math.round(Math.random() * 65) + 5;
+        salesList.push({
+            game: gameList[game], 
+            price, 
+            cost, 
+            units
+        });
+    }
+    res.send(salesList);
+})
+
+app.get("/data", async (req, res) => {
+    const mongo = await connectMongo();
+    const x = await mongo.db().collection("games").find({}).toArray()    
+    res.send(x)
+})
+
+
+app.listen(3001, async () => {
+    console.log("App running");   
+})
---
+++ mongodb.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb')
+
+const url = "mongodb://localhost:27017/game-sales-demo";
+
+let client;
+
+module.exports = async () => {
+    // this gives you client
+    // Mongoclient.connect returns promise if no callback is passed    
+    try {
+      client = await MongoClient.connect(url, { useUnifiedTopology: true });
+      return client;
+    } catch (e) {
+      console.log("Could not connect to mongodb");
+    }
+  }
+  
+  module.exports.getConnection = () => client;
+  
+  module.exports.closeConnection = () => client.close()
