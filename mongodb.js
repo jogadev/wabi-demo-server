be1839b2f504745e5ee12fdf9026b2f4ede5ef81
Jorge Garcia <jogadev@outlook.com>
2020-06-12 16:12:57 -0500

    Added unit tests and refactored application

--- mongodb.js
+++ mongodb.js
@@ -1,20 +1,21 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb");
 
 const url = "mongodb://localhost:27017/game-sales-demo";
 
 let client;
 
 module.exports = async () => {
-    // this gives you client
-    // Mongoclient.connect returns promise if no callback is passed    
-    try {
-      client = await MongoClient.connect(url, { useUnifiedTopology: true });
-      return client;
-    } catch (e) {
-      console.log("Could not connect to mongodb");
-    }
+  // this gives you client
+  // Mongoclient.connect returns promise if no callback is passed
+  try {
+    client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    console.log("Connected");
+    return client;
+  } catch (e) {
+    console.log("Could not connect to mongodb");
   }
-  
-  module.exports.getConnection = () => client;
-  
-  module.exports.closeConnection = () => client.close()
+};
+
+module.exports.getConnection = () => client;
+
+module.exports.closeConnection = () => client.close();
